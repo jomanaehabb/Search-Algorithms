d1c30a93c6216b056096e97a7ea027495afa02ed
MohamedEl-naagar <148581349+MohamedEl-naagar@users.noreply.github.com>
2024-07-06 07:57:39 +0200

    update the algorithem and fix crash cases

--- maze_solver/Main.cpp
+++ maze_solver/Main.cpp
@@ -16,12 +16,22 @@ enum Direction { NORTH, EAST, SOUTH, WEST };
 Direction currentDirection = NORTH;
 
 void turnTo(Direction newDirection) {
-    int turnCount = (newDirection - currentDirection + 4) % 4;
-    for (int i = 0; i < turnCount; i++) {
-        API::turnRight();
-    } 
+    int leftTurns = (currentDirection - newDirection + 4) % 4;
+    int rightTurns = (newDirection - currentDirection + 4) % 4;
+
+    if (leftTurns <= rightTurns) {
+        for (int i = 0; i < leftTurns; i++) {
+            API::turnLeft();
+        }
+    } else {
+        for (int i = 0; i < rightTurns; i++) {
+            API::turnRight();
+        }
+    }
+
     currentDirection = newDirection;
 }
+
 
 bool isWallInDirection(Direction dir) {
     if (dir == currentDirection) return API::wallFront();
@@ -45,8 +55,7 @@ struct Cell {
     Cell(int _x, int _y, Direction _dir) : x(_x), y(_y), dir(_dir) {}
 };
 
-void DFS(int startX, int startY)
- {
+void DFS(int startX, int startY) {
     int width = API::mazeWidth();
     int height = API::mazeHeight();
     std::vector<std::vector<bool>> visited(width, std::vector<bool>(height, false));
@@ -79,6 +88,7 @@ void DFS(int startX, int startY)
                 if (!isWallInDirection(dir)) {
                     stack.push(Cell(x, y, dir)); // Push current cell for backtracking
                     moveInDirection(dir);
+                    currentDirection = dir;
                     stack.push(Cell(newX, newY, dir));
                     visited[newX][newY] = true;
                     foundUnvisited = true;
@@ -88,11 +98,11 @@ void DFS(int startX, int startY)
         }
 
         if (!foundUnvisited) {
+            log("Backtracking from: (" + std::to_string(x) + ", " + std::to_string(y) + ")");
             if (!stack.empty()) {
                 Cell backtrack = stack.top();
-                stack.pop();
-                log("Backtracking from: (" + std::to_string(x) + ", " + std::to_string(y) + ")");
                 moveInDirection(oppositeDirection(backtrack.dir));
+                currentDirection = oppositeDirection(backtrack.dir);
             }
         }
     }
@@ -103,9 +113,7 @@ int main(int argc, char* argv[]) {
     API::setColor(0, 0, 'G');
     API::setText(0, 0, "Start");
 
-    while (true) {
-        DFS(0, 0);
-    }
+    DFS(0, 0);
 
     return 0;
 }
